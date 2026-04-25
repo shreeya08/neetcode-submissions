a2373166f6a4b1e8f3781e9a7ae1bf876af7b2ec
Shreeya <61231661+shreeya08@users.noreply.github.com>
2026-04-25 11:45:48 +0530

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.swift
@@ -0,0 +1,16 @@
+class Solution {
+//     func isAnagram(_ string1: String, _ string2: String) -> Bool {
+//     var counts1: [Character: Int] = [:]
+//     var counts2: [Character: Int] = [:]
+//     guard string1.count == string2.count else { return false }
+//     string1.forEach { counts1[$0, default: 0] += 1 }
+//     string2.forEach { counts2[$0, default: 0] += 1 }
+    
+//     return counts1 == counts2
+// }
+
+func isAnagram(_ string1: String, _ string2: String) -> Bool {
+    guard string1.count == string2.count else { return false }
+    return string1.sorted() == string2.sorted()
+}
+}
